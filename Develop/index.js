d34a1b5fa50444e212839f88a706819a5c082101
tmaraki <toshiemaraki@gmail.com>
2024-02-06 09:04:28 -0500

    update index.js with generatelogo function and initializing app. Played around with filing structures and intalled Jest. Created lib folder to hold the shapes.js files.

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,62 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-const svg = require('svg-canvas');
+console.log("Create your logo generator");
 
-inquirer
-    .prompt({
-        type:'input',
-        name: 'text',
-        message: 'Enter up to 3 characters:',
-    },
-    {
-        type:'input',
-        name:'text-color',
-        message:'Enter a color for the text:'
-    }, {
-        type:'checkbox',
-        name: 'shape',
-        message: [circle, triangle, square],
-    },
-    {
-        type:'input',
-        name: 'shape-color',
-        message: 'Enter a color for the shape:',
-    })
+const questions = [
+        {
+            type:'input',
+            name: 'text',
+            message: 'Enter up to 3 characters:',
+            validate: (input) => input.length > 0 && input.length <= 3,
+        },
+        {
+            type:'input',
+            name:'textColor',
+            message:'Enter a color for the text (keyword or hexadecimal):'
+        }, 
+        {
+            type:'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['circle', 'triangle', 'square'],
+        },
+        {
+            type:'input',
+            name: 'shapeColor',
+            message: 'Enter a color for the shape (keyword or hexadecimal):',
+        }];
+
+        // return userInput;
+    
+    console.log(generateSvg);
+
+    //svg logic based on user input
+    function generateLogo(fileName, userInput) {
+        fs.writeFileSync(fileName, userInput, (err) =>{
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('Successfully generated logo.svg!');
+            }
+        });
+    };
+
+function init() {
+    inquirer.prompt(userInput)
+    .then(function(userInput) {
+        console.log(userInput)
+        generateLogo("logo.svg", generateSvg(userInput));
+    });
+};
+
+init();
+
+
+    // run application
+//     try {
+//             const userInput = await promptUser();
+//             generateLogo(userInput);
+//         } catch (error) {
+//         console.error('Error:', error.message);
+//         }
+// };
